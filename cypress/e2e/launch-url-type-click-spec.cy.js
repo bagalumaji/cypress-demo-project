5bf57b4eed6df318aed8e4a476c1bee6d3400376
bagalumaji <bagalumaji@gmail.com>
2023-11-08 11:37:41 +0530

    test updated with assertion

--- cypress/e2e/launch-url-type-click-spec.cy.js
+++ cypress/e2e/launch-url-type-click-spec.cy.js
@@ -4,5 +4,9 @@ describe("type and click scenario", () => {
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
     );
     cy.get("a[href*='login']").last().click();
+    cy.get("#input-email").type("umajibagal23@gmail.com");
+    cy.get("#input-password").type("Umaji@123");
+    cy.get("input[value='Login']").click();
+    cy.contains("My Account");
   });
 });
