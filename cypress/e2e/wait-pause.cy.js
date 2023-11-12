1270ecd82f89fdabece099349150551ed798fedc
bagalumaji <bagalumaji@gmail.com>
2023-11-12 15:30:10 +0530

    wait and pause

---
+++ cypress/e2e/wait-pause.cy.js
@@ -0,0 +1,10 @@
+describe("wait and pause demo", () => {
+  it("wait commmand test", () => {
+    cy.visit("https://the-internet.herokuapp.com/");
+    cy.get("a[href='/horizontal_slider']").click();
+    //cy.wait(() => cy.get("h4").contains("Horizontal Slider"));
+    cy.wait(3000);
+    cy.get("h3").contains("Horizontal Slider");
+    //cy.pause(1);
+  });
+});
