55b5c7fa2da74cd09dff642057dcf8f76879cee9
bagalumaji <bagalumaji@gmail.com>
2023-12-01 09:04:31 -0800

    test

---
+++ cypress/e2e/mocha/describe.cy.js
@@ -0,0 +1,11 @@
+
+describe('suite-1',function(){
+   
+    it("test-1",function(){
+        cy.log("suite-1 test-1");
+    })
+    it("test-2",function(){
+        cy.log("suite-1 test-2");
+    })
+   
+})
