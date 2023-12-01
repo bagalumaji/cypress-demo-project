cd82e959c87b05b12de2123e0f30669010deb99a
bagalumaji <bagalumaji@gmail.com>
2023-12-01 09:05:19 -0800

    slip test

--- cypress/e2e/mocha/describe.cy.js
+++ cypress/e2e/mocha/describe.cy.js
@@ -7,5 +7,7 @@ describe('suite-1',function(){
     it("test-2",function(){
         cy.log("suite-1 test-2");
     })
-   
+    it.skip("test-3",function(){
+        cy.log("suite-1 test-3");
+    })
 })
