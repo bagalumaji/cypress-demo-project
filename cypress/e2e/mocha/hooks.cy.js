c257b4f000f348c6598fdacab4e8bf041fd625de
bagalumaji <bagalumaji@gmail.com>
2023-12-01 08:48:28 -0800

    before each

---
+++ cypress/e2e/mocha/hooks.cy.js
@@ -0,0 +1,21 @@
+
+describe('suite-1',function(){
+    beforeEach(function(){
+        cy.log("before-each-inside-describe-")
+    })
+    it("test-1",function(){
+        cy.log("suite-1 test-1");
+    })
+    it("test-2",function(){
+        cy.log("suite-1 test-2");
+    })
+})
+
+describe('suite-2',function(){
+    it("test-1",function(){
+        cy.log("suite-2 test-1");
+    })
+    it("test-2",function(){
+        cy.log("suite-2 test-2");
+    })
+})
