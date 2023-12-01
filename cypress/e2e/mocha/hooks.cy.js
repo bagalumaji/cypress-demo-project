af2ccf2786eecddaf89327ada9204b9481bebbb1
bagalumaji <bagalumaji@gmail.com>
2023-12-01 08:53:37 -0800

    before and after out descirbe

--- cypress/e2e/mocha/hooks.cy.js
+++ cypress/e2e/mocha/hooks.cy.js
@@ -1,17 +1,22 @@
-
+before(function(){
+    cy.log("before-outside-describe-")
+})
+after(function(){
+    cy.log("after-outside-describe-")
+})
 describe('suite-1',function(){
-    before(function(){
-        cy.log("before-inside-describe-")
-    })
+    // before1(function(){
+    //     cy.log("before-inside-describe-")
+    // })
     it("test-1",function(){
         cy.log("suite-1 test-1");
     })
     it("test-2",function(){
         cy.log("suite-1 test-2");
     })
-    after(function(){
-        cy.log("after-inside-describe-")
-    })
+    // after1(function(){
+    //     cy.log("after-inside-describe-")
+    // })
 })
 
 describe('suite-2',function(){
