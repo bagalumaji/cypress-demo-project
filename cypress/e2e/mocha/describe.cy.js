0e137ad930427570ddc824f7d17c6cdc4c84e705
bagalumaji <bagalumaji@gmail.com>
2023-12-01 09:05:44 -0800

    excute only test

--- cypress/e2e/mocha/describe.cy.js
+++ cypress/e2e/mocha/describe.cy.js
@@ -4,10 +4,10 @@ describe('suite-1',function(){
     it("test-1",function(){
         cy.log("suite-1 test-1");
     })
-    it("test-2",function(){
+    it.only("test-2",function(){
         cy.log("suite-1 test-2");
     })
-    it.skip("test-3",function(){
+    it("test-3",function(){
         cy.log("suite-1 test-3");
     })
 })
