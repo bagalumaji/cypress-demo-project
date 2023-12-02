83b573567963d664b76f6a31d96b2952852a15fd
bagalumaji <bagalumaji@gmail.com>
2023-12-02 03:47:14 -0800

    test case 2 view port - iphone se

--- cypress/e2e/viewport.cy.js
+++ cypress/e2e/viewport.cy.js
@@ -4,4 +4,10 @@ describe("view port tests",()=>{
         cy.wait(5000);
         cy.viewport(400,393);
     })
+
+    it.only("tc-2-viewport test for iPhone SE",()=>{
+        cy.visit("https://www.amazon.in");
+        cy.wait(5000);
+        cy.viewport("iphone-x");
+    })
 })
