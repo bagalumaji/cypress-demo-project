de8d702f5e8902f53e0d646dfe95bf7269805316
bagalumaji <bagalumaji@gmail.com>
2023-12-03 10:48:22 -0800

    added view port option in cypress.config.js

---
+++ cypress/e2e/viewport-with-width-height.cypress-config.cy.js
@@ -0,0 +1,8 @@
+describe("view port tests",()=>{
+    it("tc-1-viewport test",()=>{
+        cy.visit("https://www.amazon.in");
+    })
+    it("tc-2-viewport test",()=>{
+        cy.visit("https://www.amazon.in");
+    })
+})
