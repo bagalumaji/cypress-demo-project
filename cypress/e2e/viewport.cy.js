e92cbe54fd69571868034530c3f0376984886403
bagalumaji <bagalumaji@gmail.com>
2023-12-02 21:35:18 -0800

    viewpoet landscape

--- cypress/e2e/viewport.cy.js
+++ cypress/e2e/viewport.cy.js
@@ -1,13 +1,19 @@
 describe("view port tests",()=>{
     it("tc-1-viewport test",()=>{
         cy.visit("https://www.amazon.in");
-        cy.wait(5000);
+        cy.wait(2000);
         cy.viewport(400,393);
     })
-
-    it.only("tc-2-viewport test for iPhone SE",()=>{
+    it("tc-2-viewport test",()=>{
         cy.visit("https://www.amazon.in");
-        cy.wait(5000);
+        cy.wait(2000);
         cy.viewport("iphone-x");
     })
+
+    it("tc-3-viewport test for iPhone SE",()=>{
+        cy.visit("https://www.amazon.in");
+        cy.wait(2000);
+        cy.viewport("iphone-x","landscape");
+    })
+    
 })
