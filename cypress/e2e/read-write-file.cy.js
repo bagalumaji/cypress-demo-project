710aea8d4e5afad2c575965831a5d51132c24ba0
bagalumaji <bagalumaji@gmail.com>
2023-12-04 23:05:22 -0800

    read wright file

---
+++ cypress/e2e/read-write-file.cy.js
@@ -0,0 +1,11 @@
+describe('read-write-file',()=>{
+    it('write file test',function(){
+        cy.writeFile("testdata.txt","hello cypress");
+    })
+    it('append file test',function(){
+        cy.writeFile("testdata.txt","\nhello world of javascript ",{flag:"a+"});
+    })
+    it.only('write json file test',function(){
+        cy.writeFile("testdata.json",{data1:"data1",data2:"data2"});
+    })
+})
