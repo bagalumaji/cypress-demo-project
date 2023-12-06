49a5dbe7b1c198a6bb99487aff5511bf1ecc6097
bagalumaji <bagalumaji@gmail.com>
2023-12-06 11:19:43 -0800

    contains-with-ignore-case

--- cypress/e2e/contains-with-ingore-case.cy.js
+++ cypress/e2e/contains-with-ingore-case.cy.js
@@ -1,5 +1,5 @@
-describe('Avoid delay',function(){
-    it("avoid delay test",function(){
+describe('contains with ignore case',function(){
+    it("contains with ignore case",function(){
         cy.visit("https://www.amazon.in/");
         cy.contains("mobile",{matchCase:false}).click({force:true});
         //cy.wait(6000);
