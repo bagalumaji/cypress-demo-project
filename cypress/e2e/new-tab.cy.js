ecf1ca1b23372e3091946e8caedfe741a2248415
bagalumaji <bagalumaji@gmail.com>
2023-12-06 19:08:07 -0800

    added test case 1

--- cypress/e2e/new-tab.cy.js
+++ cypress/e2e/new-tab.cy.js
@@ -1,5 +1,6 @@
 describe('new tab',function(){
     it("new tab test",function(){
-
+        cy.visit("https://selectorshub.com/xpath-practice-page/");
+        cy.contains("user role",{matchCase:false}).invoke("removeAttr","target").click({force:true});
     })
 })
