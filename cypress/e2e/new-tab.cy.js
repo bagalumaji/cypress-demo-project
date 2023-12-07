b7c40e28e8d2e4899e43d503393b3be61f2f1b69
bagalumaji <bagalumaji@gmail.com>
2023-12-07 09:10:34 -0800

    added test case 2 with 2nd approach

--- cypress/e2e/new-tab.cy.js
+++ cypress/e2e/new-tab.cy.js
@@ -1,8 +1,14 @@
 describe('new tab',function(){
-    it("new tab test",function(){
+    it("new tab test - 1",function(){
         cy.visit("https://selectorshub.com/xpath-practice-page/");
         cy.contains("user role",{matchCase:false}).invoke("removeAttr","target").click({force:true});
         cy.wait(5000);
         cy.go("back",{force:true});
+    })
+    it.only("new tab test-2",function(){
+            cy.visit("https://selectorshub.com/xpath-practice-page/");
+            cy.contains("user role",{matchCase:false}).invoke("attr","target","_self").click({force:true});
+            cy.wait(5000);
+            cy.go("back",{force:true});
     })
 })
