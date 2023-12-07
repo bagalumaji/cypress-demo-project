83ae2b38b96962b507496aed296ed54f06242900
bagalumaji <bagalumaji@gmail.com>
2023-12-07 08:28:26 -0800

    file upload test case -2

--- cypress/e2e/file-upload.cy.js
+++ cypress/e2e/file-upload.cy.js
@@ -4,5 +4,9 @@ describe("file upload ",function (){
         cy.visit("https://document-editor.pdffiller.com/pdf-document-editor?gad_source=1&gclid=EAIaIQobChMIuqS3jNz9ggMV4V4PAh27HAHPEAAYAiAAEgKOL_D_BwE");
         cy.contains("Select from device").selectFile("testdata.txt",{action:"drag-drop"});
     });
+    it.only('file upload test case - 2', () => {
+        cy.visit("https://www.sejda.com/pdf-editor");
+        cy.get(".fileupload").selectFile("testdata.txt",{force:true});
+    });
 
 })
