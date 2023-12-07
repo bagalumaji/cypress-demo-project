76a4384a3ce8d637805f526ec48ecfadb532bb27
bagalumaji <bagalumaji@gmail.com>
2023-12-07 08:19:18 -0800

    data driven

--- cypress/e2e/ddt-test.cy.js
+++ cypress/e2e/ddt-test.cy.js
@@ -1,8 +1,10 @@
-describe("Data driven test",()=>{
-    it("data driven test",()=>{
-     cy.visit("https://demo.testfire.net/login.jsp");
-        cy.get("#uid").type("asdc");
-        cy.get("#passw").type("test123");
+describe("Data driven test", () => {
+    it("data driven test", function () {
+        cy.visit("https://demo.testfire.net/login.jsp");
+        cy.fixture("login.json").then((data) => {
+            cy.get("#uid").type(data.username);
+            cy.get("#passw").type(data.password);
+        });
         cy.get("input[value='Login']").click();
         cy.get("input[value='Login']").should("be.visible")
     })
