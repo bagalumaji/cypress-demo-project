e8953afc806e79b982fa6981ed0af802d6ad0ed7
bagalumaji <bagalumaji@gmail.com>
2023-12-14 16:46:39 +0530

    accept alert and get text of alert

--- cypress/e2e/acceptalertandgettext.cy.js
+++ cypress/e2e/acceptalertandgettext.cy.js
@@ -2,6 +2,9 @@ describe("alert handle",()=>{
     it("alert ok - test",()=>{
         cy.visit("https://the-internet.herokuapp.com/javascript_alerts");
         cy.contains('Click for JS Alert').click();
-        cy.get("#result").should("be.visible");
+        cy.on("window:alert",function (msg){
+           expect(msg).to.be.equal("I am a JS Alert");
+        });
+
     })
 })
